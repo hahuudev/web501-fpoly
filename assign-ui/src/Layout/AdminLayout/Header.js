1f26eaa7430f61a07f33b50f589e56bd7bfda927
hahudev <hahuu02dev@gmail.com>
2023-03-01 14:07:50 +0700

    fix bug json-server

--- assign-ui/src/Layout/AdminLayout/Header.js
+++ assign-ui/src/Layout/AdminLayout/Header.js
@@ -1,8 +1,5 @@
-import React from "react";
-
 function Header() {
-    return (
-        <div>
+    return `<div>
             <nav class="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
                 <div class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
                     <a class="text-white text-sm uppercase hidden lg:inline-block font-semibold" href="./index.html">
@@ -168,8 +165,7 @@ function Header() {
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        </div>`;
 }
 
 export default Header;
